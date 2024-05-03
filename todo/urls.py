595563097de03447aba58efb8a82cf7e84f7010a
Jo abek <j.xaytboyev@technocorp.uz>
2024-05-03 10:58:11 +0500

    add static and media urls

--- todo/urls.py
+++ todo/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from django.contrib import admin
 from django.urls import path, include
@@ -34,4 +35,7 @@ urlpatterns = [
     # Djoser
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken'))
-]
+] + [
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ]
